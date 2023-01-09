a6a867bfafd88b4d4c46eb1dead5f9c98b540255
majorhaji <yusufhaji1943@gmail.com>
2023-01-09 21:31:35 +0000

    styling and votes added

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -3,6 +3,10 @@ import getArticles from "../api";
 const ArticleList = () => {
   const [articles, setArticles] = useState(null);
 
+  const date = new Date();
+
+  const currentYear = date.getFullYear();
+
   useEffect(() => {
     getArticles().then((articles) => {
       setArticles(articles);
@@ -15,12 +19,19 @@ const ArticleList = () => {
   return (
     <main className="article-list">
       {articles.map((article) => {
+        const timestamp = article.created_at.slice(0,4); 
+
+         let articleAge = currentYear-  parseInt(timestamp)
+
+         articleAge > 1 ? articleAge+= ` years ago` : articleAge+= ` year ago`
         return (
           <article className="article" key={article.article_id}>
-            <h4>{article.title}</h4>
+            <h4 className="title">{article.title}</h4>
+            <aside className="votes">{article.votes}</aside>
             <section className="article-info">
-              <p>{article.author}</p>
-              <p class="created">{article.created_at}</p>
+            <p className="topic">#{article.topic}</p>
+              <p className="author">Posted by {article.author}</p>
+              <p className="created">{articleAge}</p>
             </section>
             <section className="article-body">{article.body}</section>
           </article>
