e9046cbcf384f6db75601e24815fa942a23e92c3
majorhaji <yusufhaji1943@gmail.com>
2023-01-10 09:51:50 +0000

    article new page generated

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ArticlePage from "./pages/ArticlePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/article/:article_id" element={<ArticlePage />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/ArticlePage.jsx
@@ -0,0 +1,14 @@
+import Article from "../components/Article"
+import Header from "../components/Header"
+
+const ArticlePage = () => {
+    return (
+        <div className="App">
+
+        <Header/>
+        <Article/>
+        </div>
+    )
+}
+
+export default ArticlePage;
