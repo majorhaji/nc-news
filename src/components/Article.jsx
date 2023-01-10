db6c3491562dc771356df03ddf74e1692b3039bd
majorhaji <yusufhaji1943@gmail.com>
2023-01-10 10:58:06 +0000

    comments listed

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,19 +1,23 @@
 import {useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom'
-import {getArticleById} from '../api'
+import {getArticleById, getCommentsById} from '../api'
 
 const Article = () => {
 
     let {article_id} = useParams();
 
-    const [article, setArticle] = useState(null);
+    const [article, setArticle] = useState([]);
     const [isLoading, setLoading] = useState(true);
+    const [comments, setComments] = useState([])
 
     useEffect(() => {
         setLoading(true);
         getArticleById(article_id).then(({article}) => {
-            setArticle(article);
-            setLoading(false);
+          setArticle(article);
+          return getCommentsById(article_id)  
+        }).then(({comments}) => {
+          setComments(comments)
+          setLoading(false);
         }).catch(err => console.log(err))
     }, [])
     
@@ -37,6 +41,22 @@ const Article = () => {
                   <p className="created">{article.created_at}</p>
                 </section>
                 <section className="article-body">{article.body}</section>
+                <p className="comment-count">{article.comment_count} comments</p>
+              </article>
+              <article className="comments">
+               
+                {comments.map((comment) => {
+                  return(
+                    <section className='comment' key={comment.comment_id}>
+                      <aside className="votes">{comment.votes}</aside>
+                      <section className="comment-info">
+                      <p className="author">{comment.author}</p>
+                  <p className="created">{comment.created_at}</p>
+                      </section>
+                      <p className="comment-body">{comment.body}</p>
+                    </section>
+                  )
+                })}
               </article>
         </main>
       );
