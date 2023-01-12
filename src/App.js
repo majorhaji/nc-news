16f5c8e4e3942412c20235e395d8b8d078908579
majorhaji <yusufhaji1943@gmail.com>
2023-01-12 13:54:33 +0000

    topicnav persistence

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ArticlePage from "./pages/ArticlePage";
 import TopicPage from "./pages/TopicPage";
+import Header from "./components/Header";
+import TopicNav from "./components/TopicNav";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
+      <TopicNav />
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/article/:article_id" element={<ArticlePage />}></Route>
